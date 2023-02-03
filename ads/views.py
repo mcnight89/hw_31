1e8ae038a57df59bce67ae86e95f8161088a3660
mcnight89 <mc.night89@gmail.com>
2023-02-03 23:49:11 +0300

    2

--- ads/views.py
+++ ads/views.py
@@ -1,5 +1,8 @@
+import json
+
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 from ads.models import Category, Ad
 
@@ -9,6 +12,7 @@ def start_page(request):
     return HttpResponse('200 {"status": "ok"}')
 
 
+@csrf_exempt
 def category_all(request):
     if request.method == "GET":
         cat = Category.objects.all()
@@ -22,6 +26,18 @@ def category_all(request):
 
         return JsonResponse(response, safe=False, json_dumps_params={'ensure_ascii': False})
 
+    elif request.method == "POST":
+        cat_data = json.loads(request.body)
+
+        category = Category()
+        category.name = cat_data['name']
+        category.save()
+
+        return JsonResponse({
+            'id': category.id,
+            'name': category.name
+        })
+
 
 def get_category_id(request, category_id):
     if request.method == "GET":
@@ -33,6 +49,7 @@ def get_category_id(request, category_id):
         })
 
 
+@csrf_exempt
 def ad_all(request):
     if request.method == "GET":
         ad = Ad.objects.all()
@@ -51,19 +68,39 @@ def ad_all(request):
 
         return JsonResponse(response, safe=False, json_dumps_params={'ensure_ascii': False})
 
+    elif request.method == "POST":
+        ad_data = json.loads(request.body)
+
+        ads = Ad()
+        ads.name = ad_data['name']
+        ads.author = ad_data['author']
+        ads.price = ad_data['price']
+        ads.description = ad_data['description']
+        ads.address = ad_data['address']
+        ads.is_published = ad_data['is_published']
+        ads.save()
+
+        return JsonResponse({
+            'id': ads.id,
+            'name': ads.name,
+            'author': ads.author,
+            'price': ads.price,
+            'description': ads.description,
+            'address': ads.address,
+            'is_published': ads.is_published
+        })
+
 
 def get_ad_id(request, ad_id):
     if request.method == "GET":
         ad = Ad.objects.get(pk=ad_id)
 
         return JsonResponse({
-                'id': ad.id,
-                'name': ad.name,
-                'author': ad.author,
-                'price': ad.price,
-                'description': ad.description,
-                'address': ad.address,
-                'is_published': ad.is_published
+            'id': ad.id,
+            'name': ad.name,
+            'author': ad.author,
+            'price': ad.price,
+            'description': ad.description,
+            'address': ad.address,
+            'is_published': ad.is_published
         })
-
-
